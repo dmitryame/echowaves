09e1c9c0d5914759779c7111324c331b4c341b29
dmitryame <dmitryme@gmail.com>
2008-10-02 19:51:23 -0400

    fixed site_keys for deployement

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ namespace :deploy do
   task :copy_prod_configuration do
     run "cp /u/config/#{application}/database.yml #{release_path}/config/"
     run "cp /u/config/#{application}/environment.rb #{release_path}/config/"
-    run "cp /u/config/#{application}/site_key.rb #{release_path}/config/initializers"
+    run "cp /u/config/#{application}/site_keys.rb #{release_path}/config/initializers"
   end
   
   after "deploy:update_code", "deploy:copy_prod_configuration"

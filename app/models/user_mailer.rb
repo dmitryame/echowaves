ae3f48e1d516aa2f90a7d0ca0662e29e52267f53
dmitryame <dmitryme@gmail.com>
2008-10-02 19:15:17 -0400

    initial something that can be deployed

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://echowaves.com/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://echowaves.com/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "admin@echowaves.com"
+      @subject     = "[echowaves.com] "
       @sent_on     = Time.now
       @body[:user] = user
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
+  map.home '/', :controller => "sessions", :action => "new"
+  
+  
   map.resources :users
 
   map.resource :session

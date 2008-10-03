acfa5c0025f2fc1c181d87f78284bfdd6dafbc9a
dmitryame <dmitryme@gmail.com>
2008-10-03 16:38:20 -0400

    added db sessions, added shoulda plugin and factory_girl plugin

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081003193147) do
+ActiveRecord::Schema.define(:version => 20081003203731) do
 
   create_table "conversations", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40

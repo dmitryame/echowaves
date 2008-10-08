69c67b30225745d6279fe19fd0c13a337e864c0c
dmitryame <dmitryme@gmail.com>
2008-10-07 23:02:38 -0400

    html links gsubed with a link tag, added a space to separate from the <br/> tag

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,7 @@ module MessagesHelper
 
   def mark_up(message)
     parts = message.split "\n"
-    parts.map {|s| h(s)}.join("<br/>").gsub(/(http|https|ftp)([^ ]+)/i) {|s|  "<a href='#{s}'>#{s}</a>"}        
+    parts.map {|s| h(s)}.join(" <br/>").gsub(/(http|https|ftp)([^ ]+)/i) {|s|  "<a href='#{s}'>#{s}</a>"}        
   end
 
 end

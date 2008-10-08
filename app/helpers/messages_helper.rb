6345f9b23fcaff9f447c1e61dd4777afaa88318b
Jason Rogers <jacaetevha@gmail.com>
2008-10-08 12:32:26 -0400

    BlueCloth

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,8 +1,12 @@
 module MessagesHelper
 
-  def mark_up(message)
+  def xx_mark_up(message)
     parts = message.split "\n"
     parts.map {|s| h(s)}.join("<br/>")
   end
 
+  def mark_up(message)
+    require 'bluecloth'
+    BlueCloth::new( message ).to_html
+  end
 end

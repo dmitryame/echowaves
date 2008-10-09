d17a15a14a61ba65df2f486375f62f33e904b21e
phanes <phanes@cam-phanes2.rmg-ny.com>
2008-10-09 16:05:08 -0400

    make sure environment.rb knows to yell about gravtastic gem

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem "gravtastic"
+
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named

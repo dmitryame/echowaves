0d14563e771c5495f86bbaf8eb60015f4f4a44f9
phanes <phanes@cam-phanes2.rmg-ny.com>
2008-10-09 17:26:21 -0400

    add BlueCloth dependency

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem "gravtastic"
+  config.gem "BlueCloth"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.

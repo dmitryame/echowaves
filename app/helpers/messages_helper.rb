8299748286aea84eea1a7fc160841a1033ba7de4
dmitryame <dmitryme@gmail.com>
2008-10-09 19:31:55 -0400

    removed bluecloth mark_up for now

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,11 +1,11 @@
 module MessagesHelper
 
-  def xx_mark_up(message)
+  def mark_up(message)
     parts = message.split "\n"
     parts.map {|s| h(s)}.join(" <br/>").gsub(/(http|https|ftp)([^ ]+)/i) {|s|  "<a href='#{s}'>#{s}</a>"}        
   end
 
-  def mark_up(message)
+  def xx_mark_up(message)
     require 'bluecloth'
     BlueCloth::new( message ).to_html
   end

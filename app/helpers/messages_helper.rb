a8fe245bcd59a40e291c2a4a195a7ba66a6af787
dmitryame <dmitryme@gmail.com>
2008-10-14 09:09:14 -0400

    open link in a new window

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,7 @@ module MessagesHelper
 
   def mark_up(message)
     parts = message.split "\n"
-    parts.map {|s| h(s)}.join(" <br/>").gsub(/(http|https|ftp)([^ ]+)/i) {|s|  "<a href='#{s}'>#{s}</a>"}        
+    parts.map {|s| h(s)}.join(" <br/>").gsub(/(http|https|ftp)([^ ]+)/i) {|s|  "<a href='#{s}' TARGET=_BLANK>#{s}</a>"}        
   end
 
   def xx_mark_up(message)

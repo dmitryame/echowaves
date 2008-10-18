791f55ca2f1156ef8f49afa5e186abd161bed460
dmitryame <dmitryme@gmail.com>
2008-10-17 23:25:09 -0400

    same as before

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,14 @@
 module ApplicationHelper  
   def orbited_javascript
     [
-    "<script src=\"http://#{ORBITED_HOST}:#{ORBITED_PORT}/static/Orbited.js\"></script>",
+    "<script src=\"http://#{ORBITED_HOST}:#{ORBITED_PORT}/orbited/static/Orbited.js\"></script>",
     '<script>',
     '  document.domain = document.domain;',
     "  Orbited.settings.port = #{ORBITED_PORT};",
     "  Orbited.settings.hostname = '#{ORBITED_HOST}';",
     '  TCPSocket = Orbited.TCPSocket;',
     '</script>',
-    "<script src=\"http://#{ORBITED_HOST}:#{ORBITED_PORT}/static/protocols/stomp/stomp.js\"></script>"
+    "<script src=\"http://#{ORBITED_HOST}:#{ORBITED_PORT}/orbited/static/protocols/stomp/stomp.js\"></script>"
     ].join("\n")
   end
 end

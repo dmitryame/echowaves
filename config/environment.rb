9bf7a0d680ae8b8125f27b13370ddaf21a0dcf18
phanes <phanes@cam-phanes2.rmg-ny.com>
2008-10-20 18:38:21 -0400

    tell environment.rb that we need the stomp gem

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem "gravtastic"
 #  config.gem "BlueCloth"
   config.gem "paperclip"
+  config.gem "stomp"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.

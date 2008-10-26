02785409d7f5311f7b450ed6c0b48e26e4e6705c
dmitryame <dmitryme@gmail.com>
2008-10-26 14:21:51 -0400

    added users view that includes users conversations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
+  before_filter :login_required
+
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   
+  # GET /conversations
+  # GET /conversations.xml
+  def index
+    @users = User.paginate :page => params[:page], :order => 'login DESC'
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+
+  # GET /clients/1
+  # GET /clients/1.xml
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
 
   # render new.rhtml
   def new
@@ -46,6 +69,11 @@ class UsersController < ApplicationController
        @user = User.find(session[:user_id])    
      end
    end
+
+
+
+
+
 
    # Change password action  
    def change_password

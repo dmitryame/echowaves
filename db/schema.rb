5178dd48970faab91e240600a32c1ce1062279f6
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-11-08 21:10:12 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081108173425) do
+ActiveRecord::Schema.define(:version => 20081108184556) do
 
   create_table "conversations", :force => true do |t|
     t.string   "name"

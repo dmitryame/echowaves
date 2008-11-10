d34284bc22dba27f8c8fef445ba3e97d3430066c
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-11-10 01:21:27 +0100

    Let to the users to chose their time zones during signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :time_zone
 
   is_gravtastic :size => 40, :default => "identicon" # "monsterid" or "identicon", or "wavatar"
 

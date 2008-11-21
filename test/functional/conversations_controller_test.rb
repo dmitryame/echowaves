821e0c79a876dcce066e0e801a68d7607378e0a8
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-11-21 14:10:28 +0100

    Tests for makewriteable and makereadonly actions

--- test/functional/conversations_controller_test.rb
+++ test/functional/conversations_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ConversationsControllerTest < ActionController::TestCase
   def setup
     #authenticate
-    create_user_and_authenticate
+    @current_user = create_user_and_authenticate
     @conversation = Factory(:conversation)
     @controller = ConversationsController.new
   end
@@ -20,6 +20,46 @@ class ConversationsControllerTest < ActionController::TestCase
       :create#, 
       # :destroy
       ]    
-  end        
-
+  end
+  
+  context "makereadonly action" do
+    setup do
+      @conversation = Factory.create(:conversation)
+    end
+    
+    should "not make the conversation readonly if the current_user is not the owner" do
+      @owner = Factory.create(:user, :login => "user1")
+      @message1 = Factory.create(:message, :conversation => @conversation, :user => @owner)
+      put :makereadonly, :id => @conversation
+      assert_equal false, assigns(:conversation).read_only
+    end
+    
+    should "make the conversation readonly if the current_user is the owner" do
+      @message1 = Factory.create(:message, :conversation => @conversation, :user => @current_user)      
+      put :makereadonly, :id => @conversation
+      assert_equal true, assigns(:conversation).read_only
+    end  
+  end
+  
+  context "makewritable action" do
+    setup do
+      @conversation = Factory.create(:conversation)
+    end
+    
+    should "not make the conversation writeable if the current_user is not the owner" do
+      @owner = Factory.create(:user, :login => "user1")
+      @message1 = Factory.create(:message, :conversation => @conversation, :user => @owner)
+      @conversation.update_attributes(:read_only => true)
+      put :makewriteable, :id => @conversation
+      assert_equal true, assigns(:conversation).read_only
+    end
+    
+    should "make the conversation writeable if the current_user is the owner" do
+      @message1 = Factory.create(:message, :conversation => @conversation, :user => @current_user)      
+      @conversation.update_attributes(:read_only => true)
+      put :makewriteable, :id => @conversation
+      assert_equal false, assigns(:conversation).read_only
+    end  
+  end
+  
 end

8ab2e871248699c9d7b19400154f32ac3b8cced6
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-11-26 21:30:25 +0100

    Show new messages count in dock icon (fluidapp, initial implementation)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Array.prototype.sum = function() {
+  return (! this.length) ? 0 : this.slice(1).sum() +
+      ((typeof this[0] == 'number') ? this[0] : 0);
+};
+
+function ShowUnreadMessagesInFLuidapp()
+{
+  var numbers = $$('.msgcount').collect(function(n) {
+    return parseInt(n.innerHTML);
+  });
+  window.fluid.dockBadge = numbers.sum();
+}

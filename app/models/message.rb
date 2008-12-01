151da0fc08000b5903135d06fa3e2da59a4020b6
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2008-11-30 20:28:22 -0500

    big refactoring and simplification of the message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,56 +2,40 @@ class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
   
+  # FIXME: should this really have a double association to the same model?
   has_many :abuse_reports
-
   belongs_to :abuse_report
   
   has_many :conversations, # these are the conversations spawned from the message
-  :foreign_key => "parent_message_id"
+           :foreign_key => "parent_message_id"
 
   has_attached_file :attachment,
-  :styles => {
-    :thumb => "64x64>",
-    :small => "150x150>",
-    :big   => "400x400>" 
-  },
-  :path => PAPERCLIP_PATH,
-  :url  => PAPERCLIP_URL
+    :styles => {
+      :thumb => "64x64>",
+      :small => "150x150>",
+      :big   => "400x400>" 
+    },
+    :path => PAPERCLIP_PATH,
+    :url  => PAPERCLIP_URL
   
-  named_scope :published,
-              :conditions => { :abuse_report_id => nil }
-  
+  named_scope :published, :conditions => { :abuse_report_id => nil }
   
   validates_attachment_size :attachment, :less_than => 5.megabytes
   validates_attachment_content_type :attachment, :content_type => [ 'application/pdf', 'application/x-pdf', 'application/x-download', 'application/rtf', 'image/gif', 'image/jpeg', 'image/png', 'image/tiff', 'image/rgb' ]
   
-  validates_presence_of     :message
+  validates_presence_of :user_id, :conversation_id, :message
 
-  validates_presence_of :user_id, :conversation_id
-
-  #expected to return a new spawned conversation
+  # expected to return a new spawned conversation
   def spawn_new_conversation(user)
-    convo = Conversation.new
-    convo.user_id = user.id  
-    convo.name = (user.login + " spawned from: " + message)[0,100] # the convo name length is limited to 100, do not remove this range -- will fails otherwise
-    convo.description = self.message
-    convo.parent_message_id = self.id
-    convo.save
-    convo 
+    name = (user.login + " spawned from: " + message)[0,100] # FIXME: the convo name length is limited to 100, do not remove this range -- will fails otherwise
+    user.conversations.create(:description => self.message, :parent_message_id => self.id, :name => name)
   end
 
   def after_create 
-    subscription = Subscription.find(:first, :conditions => ["user_id = ? and conversation_id = ?", user.id, conversation.id])     
-    # # find_by_user_and_conversation(:user => user, :conversation => conversation)
-    # 
-    if subscription == nil
-      subscription = Subscription.new
-      subscription.user_id = user.id
-      subscription.conversation_id = conversation.id
-      subscription.save      
+    unless subscription = Subscription.find_by_user_id_and_conversation_id(user.id, conversation.id)
+      subscription = user.subscriptions.create(:conversation => conversation) 
     end
     subscription.mark_read
-    # puts "new subscription: user:" + user.id.to_s + " conversation:" + conversation.id.to_s
   end
 
   

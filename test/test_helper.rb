637e73d77443cc936de7a1613ecfd69c4e085615
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2008-12-02 21:19:32 -0500

    added shoulda macro for testing paperclip attachments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,5 +26,19 @@ class Test::Unit::TestCase
     @user
   end
 
+  def self.should_have_attached_file(attachment)
+    klass = self.name.gsub(/Test$/, '').constantize
 
+    context "To support a paperclip attachment named #{attachment}, #{klass}" do
+      should_have_db_column("#{attachment}_file_name",    :type => :string)
+      should_have_db_column("#{attachment}_content_type", :type => :string)
+      should_have_db_column("#{attachment}_file_size",    :type => :integer)
+    end
+
+    should "have a paperclip attachment named ##{attachment}" do
+      assert klass.new.respond_to?(attachment.to_sym), 
+             "@#{klass.name.underscore} doesn't have a paperclip field named #{attachment}"
+      assert_equal Paperclip::Attachment, klass.new.send(attachment.to_sym).class
+    end
+  end
 end

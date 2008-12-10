13025069bb682d1ac3eda77228623d2ddc8d8260
D <dmitry@Macintosh.local>
2008-12-09 21:11:19 -0500

    proper initialization of counter cache

--- db/migrate/20081210013209_add_message_counter_to_convo.rb
+++ db/migrate/20081210013209_add_message_counter_to_convo.rb
@@ -2,7 +2,10 @@ class AddMessageCounterToConvo < ActiveRecord::Migration
   def self.up
     add_column :conversations, :messages_count, :integer, :default => 0 
     
-    Conversation.find(:all).map {|convo| puts convo.messages(:refresh).size}    
+    Conversation.reset_column_information
+    Conversation.find(:all).each do |convo| 
+      Conversation.update_counters convo.id, :messages_count => convo.messages.count
+    end
   end
 
   def self.down

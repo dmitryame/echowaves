0b434d24e78eade184bbcbd3a14510e604547d39
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-12-10 12:59:47 +0100

    Resize messages text area to fit with the content, but don't hide the messages behind. Also don't allow to grow the text area out of the screen.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,3 +13,22 @@ function ShowUnreadMessagesInFLuidapp()
   });
   window.fluid.dockBadge = numbers.sum();
 }
+
+function FitToTextAndMoveMessagesUp(id, maxHeight)
+{
+   var text = id && id.style ? id : document.getElementById(id);
+   if ( !text )
+      return;
+
+   var adjustedHeight = text.clientHeight;
+   if ( !maxHeight || maxHeight > adjustedHeight )
+   {
+      adjustedHeight = Math.max(text.scrollHeight, adjustedHeight);
+      if ( maxHeight )
+         adjustedHeight = Math.min(maxHeight, adjustedHeight);
+      if ( adjustedHeight > text.clientHeight ) {
+         text.style.height = adjustedHeight + "px";
+         $('messages').setStyle("padding-bottom: " + (adjustedHeight+80) + "px");
+      }
+   }
+}

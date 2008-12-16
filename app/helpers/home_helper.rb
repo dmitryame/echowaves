4b4f7452429b75152115c03408f80a5ba80a2b88
D <dmitry@Macintosh.local>
2008-12-15 20:54:52 -0500

    issue 37, home menu for logged in users points to personal convo

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def home_menu_item
+    if !logged_in?
+      link_to t('ui.home'), home_path
+    else
+      link_to t('ui.home'), conversation_messages_path(current_user.personal_conversation)
+    end
+  end
 end

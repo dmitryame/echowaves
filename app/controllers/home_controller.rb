6838d6486ba900c8bc2ee9ec2163139e6b438567
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-12-17 14:50:16 +0100

    No more conversations in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   # just a homepage
   def index
-    @conversation = begin Conversation.find(HOME_CONVERSATION); rescue; end
-    @messages = begin @conversation.messages.published.find(:all, :include => [:user], :limit => 30, :order => 'id DESC'); rescue; end
   end
   
   def terms

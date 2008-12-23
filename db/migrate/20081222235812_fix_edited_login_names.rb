50ff6313d34637223d956c1c98d9634134049ad6
dmitryame <dmitry@custommode.com>
2008-12-22 19:34:54 -0500

    user might not have a personal convo if not activated yet

--- db/migrate/20081222235812_fix_edited_login_names.rb
+++ db/migrate/20081222235812_fix_edited_login_names.rb
@@ -1,7 +1,7 @@
 class FixEditedLoginNames < ActiveRecord::Migration
   def self.up
     User.all.each do |u|
-      u.update_attributes( :login => u.personal_conversation.name ) unless u.login == u.personal_conversation.name
+      u.update_attributes( :login => u.personal_conversation.name ) unless u.personal_conversation != nil and u.login == u.personal_conversation.name
     end
   end
 

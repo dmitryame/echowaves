abdf713c125e7cb15238436a0bb77a5238dc9a76
dmitryame <dmitry@custommode.com>
2008-12-22 19:44:58 -0500

    the conditions were reverced, getting tired -- late in a day

--- db/migrate/20081222235812_fix_edited_login_names.rb
+++ db/migrate/20081222235812_fix_edited_login_names.rb
@@ -1,7 +1,7 @@
 class FixEditedLoginNames < ActiveRecord::Migration
   def self.up
     User.all.each do |u|
-      u.update_attributes( :login => u.personal_conversation.name ) unless (u.personal_conversation != nil && u.login == u.personal_conversation.name)
+      u.update_attributes( :login => u.personal_conversation.name ) unless (u.personal_conversation == nil || u.login == u.personal_conversation.name)
     end
   end
 

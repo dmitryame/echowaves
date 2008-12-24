91281abd86b7af04323aa1e9ae771f6681f88c31
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-12-24 17:38:08 +0100

    Better layout for pdf attachments

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -15,9 +15,9 @@ module MessagesHelper
       '<div class="img_attachment">' + link_to( image_tag( message.attachment.url(:big) ), message.attachment.url, :target => '_blank' ) + "</div>"
     elsif message.has_pdf?
       %Q(
-        #{link_to( image_tag( 'icons/pdf_large.jpg', :alt => 'PDF Document' ), message.attachment.url, :target => '_blank' )}
-        <br />
-        #{link_to( message.attachment_file_name, message.attachment.url, :target => '_blank' )}
+        <div class="file_attachment">
+        #{link_to( image_tag( 'icons/pdf_large.jpg', :alt => 'PDF Document', :width => 100 ), message.attachment.url, :target => '_blank' )}
+        </div>
       )
     end
   end

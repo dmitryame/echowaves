f53eba5fa14fd45c23cd90b12ddfda8334d40429
dmitryame <dmitry@custommode.com>
2008-12-25 11:04:42 -0500

    fixed deprecation warning related to to_a

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,7 +55,7 @@ class User < ActiveRecord::Base
   
   #this returns friends convos
   def friends_convos
-    self.subscribed_conversations.published.personal - self.personal_conversation.to_a
+    self.subscribed_conversations.published.personal - [self.personal_conversation]
   end
 
   #this returns friends (users)

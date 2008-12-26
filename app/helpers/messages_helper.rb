f64c9de997408a15538a13584dc1f93b890a4b5c
dmitryame <dmitry@custommode.com>
2008-12-25 20:28:16 -0500

    added message text to alt attribute for image

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -13,7 +13,7 @@ module MessagesHelper
 
   def display_attachment(message)
     if message.has_image?
-      '<div class="img_attachment">' + link_to( image_tag( message.attachment.url(:big) ), message.attachment.url, :target => '_blank' ) + "</div>"
+      '<div class="img_attachment">' + link_to( image_tag( message.attachment.url(:big), :alt => message.message ), message.attachment.url, :target => '_blank') + "</div>"
     elsif message.has_pdf?
       %Q(
         <div class="file_attachment">

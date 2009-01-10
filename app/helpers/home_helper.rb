bb5b15e618ca3699f9b977dbed4e8d67970a7693
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-01-10 16:33:44 +0100

    Add a tooltip to the home link when the user is loged, so the user can know the login name of the current user and the personal convo

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,7 +4,10 @@ module HomeHelper
     if !logged_in?
       link_to t('ui.home'), home_path
     else
-      link_to t('ui.home'), conversation_messages_path(current_user.personal_conversation)
+      link_to t('ui.home'),
+        conversation_messages_path(current_user.personal_conversation),
+        :class => 'tip', 
+	    	:title => "personal convo for " + current_user.login
     end
   end
   

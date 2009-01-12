1eec4077e4d3c85d06daf32cd15fab8c4361ab6e
dmitryame <dmitryme@gmail.com>
2009-01-12 17:20:50 -0500

    some top nav improovements

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,2 @@
 module HomeHelper
-  
-  def home_menu_item
-    if !logged_in?
-      link_to t('ui.home'), home_path
-    else
-      link_to t('ui.home'),
-        conversation_messages_path(current_user.personal_conversation),
-        :class => 'tip', 
-	    	:title => "personal convo for " + current_user.login
-    end
-  end
-  
 end

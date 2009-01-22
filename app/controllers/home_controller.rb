dbb5526e41fd15eaadd17b36a079d0e6a27541c2
dmitryame <dmitry@custommode.com>
2009-01-21 22:41:41 -0500

    changed the number of recently joined users to 20

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   # just a homepage
   def index
-    @users = User.find(:all, :order => 'created_at DESC', :limit => 18, :conditions => "activated_at is not null")
+    @users = User.find(:all, :order => 'created_at DESC', :limit => 20, :conditions => "activated_at is not null")
     @convos = Conversation.find(:all, :order => 'created_at DESC', :limit => 10, :conditions => "abuse_report_id is null and personal_conversation = 0")
   end
   

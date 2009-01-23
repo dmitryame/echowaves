ea32b0574e2ffa872ee4bd5368c4ad7684e1389d
dmitryame <dmitry@custommode.com>
2009-01-22 21:38:31 -0500

    showing 56 recent users on the home page -- looks better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   # just a homepage
   def index
-    @users = User.find(:all, :order => 'created_at DESC', :limit => 50, :conditions => "activated_at is not null")
+    @users = User.find(:all, :order => 'created_at DESC', :limit => 56, :conditions => "activated_at is not null")
     @recent_convos = Conversation.find(:all, :order => 'created_at DESC', :limit => 10, :conditions => "abuse_report_id is null and personal_conversation = 0")
     @popular_convos = ConversationVisit.find(:all, :group => :conversation_id, :order => "visits_count DESC", :limit => 10).map { |convo_visit| convo_visit.conversation }    
 

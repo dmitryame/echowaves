78d1b59b5342296391e29e43f4c7dd8b7d7f20b6
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-02 14:31:58 +0100

    Change class of test_helper for rails 2.3+

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 %w(test_help factory_girl).each { |lib| require lib }
 require File.expand_path(File.dirname(__FILE__) + "/factories")
 
-class Test::Unit::TestCase
+class ActiveSupport::TestCase
   include AuthenticatedTestHelper
 
   self.use_transactional_fixtures = true

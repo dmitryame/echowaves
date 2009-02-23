769180628e38439b95800500ef99b5e6d5b8a562
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-23 02:42:56 +0100

    Fix migration problem

--- db/migrate/20090221150550_add_persistence_token_field_to_users_table.rb
+++ db/migrate/20090221150550_add_persistence_token_field_to_users_table.rb
@@ -1,9 +1,6 @@
 class AddPersistenceTokenFieldToUsersTable < ActiveRecord::Migration
   def self.up
     add_column :users, :persistence_token, :string, :nil => false
-    User.find(:all).each do |u|
-      u.reset_persistence_token!
-    end
   end
 
   def self.down

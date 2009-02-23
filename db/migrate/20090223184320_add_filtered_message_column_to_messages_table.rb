af87469601094817e715694085a5f0ad8f53fa2c
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-23 21:48:07 +0100

    Disable validations so migration can pass without problems

--- db/migrate/20090223184320_add_filtered_message_column_to_messages_table.rb
+++ db/migrate/20090223184320_add_filtered_message_column_to_messages_table.rb
@@ -1,4 +1,16 @@
 class AddFilteredMessageColumnToMessagesTable < ActiveRecord::Migration
+  # disable validations so the migration will pass
+  class Message < ActiveRecord::Base
+    auto_html_for(:message) do
+      html_escape
+      youtube(:width => 400, :height => 250)
+      vimeo
+      image
+      link(:target => "_blank", :rel => "nofollow")
+      simple_format
+    end
+  end
+  
   def self.up
     add_column :messages, :message_html, :text
     # generate all the markup for the existing messages

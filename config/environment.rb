035772a322305edcc21d9f98be6b723e4a3545fc
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-23 04:01:46 +0100

    Use authlogic plugin instead the gem, troubleshooting deployment problem

--- config/environment.rb
+++ config/environment.rb
@@ -27,10 +27,10 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rack", :version => '0.9.1'
   config.gem "gravtastic"
-#  config.gem "BlueCloth"
+  # config.gem "BlueCloth"
   # config.gem "paperclip"
   config.gem "stomp"
-  config.gem "authlogic"
+  # config.gem "authlogic"
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
--- vendor/plugins/authlogic/lib/authlogic/orm_adapters/active_record_adapter/acts_as_authentic/session_maintenance.rb
+++ vendor/plugins/authlogic/lib/authlogic/orm_adapters/active_record_adapter/acts_as_authentic/session_maintenance.rb
@@ -40,11 +40,9 @@ module Authlogic
           
                   #{options[:session_ids].inspect}.each do |session_id|
                     session = #{options[:session_class]}.find(session_id, self)
-                    if session
-                      if !session.record.blank?
-                        @_logged_out = false
-                        @_sessions << session if session.record == self
-                      end
+                    if session && !session.record.blank?
+                      @_logged_out = false
+                      @_sessions << session if session.record == self
                     end
                   end
                 end
@@ -61,13 +59,9 @@ module Authlogic
                   # We only want to automatically login into the first session, since this is the main session. The other sessions are sessions
                   # that need to be created after logging into the main session.
                   session_id = #{options[:session_ids].inspect}.first
-          
-                  # If we are already logged in, ignore this completely. All that we care about is updating ourself.
-                  next if #{options[:session_class]}.find(session_id, self)
-                        
-                  # Log me in
-                  args = [self, session_id].compact
-                  #{options[:session_class]}.create(*args)
+                   
+                  # Log me in, only if we aren't already logged in
+                  #{options[:session_class]}.create(*[self, session_id].compact) if !#{options[:session_class]}.find(session_id, self)
                 end
         
                 def update_sessions!

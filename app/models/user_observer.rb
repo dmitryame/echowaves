651512d5d0b796cd788453dfd3e3c8f4fdaa82e4
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-23 04:15:41 +0100

    Remove unused callbacks

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -4,10 +4,10 @@ class UserObserver < ActiveRecord::Observer
     UserMailer.deliver_signup_notification(user)
   end
 
-  def after_save(user)
-    UserMailer.deliver_activation(user) if user.recently_activated?
-    UserMailer.deliver_forgot_password(user) if user.recently_forgot_password?
-    UserMailer.deliver_reset_password(user) if user.recently_reset_password?   
-  end
+  # def after_save(user)
+  #   UserMailer.deliver_activation(user) if user.recently_activated?
+  #   UserMailer.deliver_forgot_password(user) if user.recently_forgot_password?
+  #   UserMailer.deliver_reset_password(user) if user.recently_reset_password?   
+  # end
   
 end

f4ae4861641c73a3bd23cd3d6d4bae12bf0f48dd
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-25 23:00:24 +0100

    Remove some unused named routes and clean the routes.rb file a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :user_session
-  map.resources :password_resets
   
-  map.resources :users, :member => {
-    :tagged_convos => :get
-  }
+  map.resource  :user_session
+  map.resources :password_resets
+  map.resource  :msgsearch, :only => [:show, :create]
+  map.resource  :convosearch, :only => [:show, :create]
+  map.resources :users, :member => { :tagged_convos => :get }
 
   map.resources :conversations, :member => {
     :private_status     => :put,
@@ -18,68 +18,22 @@ ActionController::Routing::Routes.draw do |map|
     :invite             => :get,
     :invite_from_list   => :post
     }, :new => { :spawn => :get } do |conversation|
-    conversation.resources :messages,
-    :member => {
-      :report => :post
-    }, :except => [:edit, :update, :destroy]
-  end
+      conversation.resources :messages, :member => { :report => :post }, 
+                                        :except => [:edit, :update, :destroy]
+    end
   
-  map.resource :msgsearch, :only => [:show, :create]
-  map.resource :convosearch, :only => [:show, :create]
-
+  
   map.home '/', :controller => "home", :action => "index"
-  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
-  map.activate '/activate/:perishable_token', :controller => "users", :action => "activate"
   map.login '/login', :controller => 'user_sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
+  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.forgot_password '/forgot_password', :controller => "users", :action => "forgot_password"
-  map.reset_password '/reset_password/:id', :controller   => "users", :action => "reset_password" 
+  map.activate '/activate/:perishable_token', :controller => "users", :action => "activate"
   
   map.custom_styles '/stylesheets/custom.css', :controller => 'users', :action => "styles", :format => "css"
 
   map.complete_conversation_name '/complete_conversation_name', :controller => 'conversations', :action => "complete_name"
   map.complete_user_name '/complete_user_name', :controller => 'users', :action => "complete_name"
   
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end

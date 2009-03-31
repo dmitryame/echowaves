1f70376b81bee852cf9f71a66e2a58b7cfc46ba9
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-03-31 21:28:46 +0200

    Add single_access_token column

---
+++ db/migrate/20090330195815_add_single_access_token_to_users.rb
@@ -0,0 +1,9 @@
+class AddSingleAccessTokenToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :single_access_token, :string,  :null => false
+  end
+
+  def self.down
+    remove_column :users, :single_access_token
+  end
+end

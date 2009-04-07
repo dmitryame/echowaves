0567ddb793836af1a026e7edcb574db39809132a
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-04-07 22:04:04 +0200

    Create a single_access_token for all existing users

--- db/migrate/20090330195815_add_single_access_token_to_users.rb
+++ db/migrate/20090330195815_add_single_access_token_to_users.rb
@@ -1,6 +1,12 @@
 class AddSingleAccessTokenToUsers < ActiveRecord::Migration
+  
+  class User < ActiveRecord::Base; acts_as_authentic; end;
+  
   def self.up
     add_column :users, :single_access_token, :string,  :null => false
+    User.all.each do |user|  
+      user.reset_single_access_token!
+    end
   end
 
   def self.down

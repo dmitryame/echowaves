33f39e4e6e38bf81abb42ba18e15856300db0d05
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-05-14 09:52:58 +0200

    Fix followers notifications test

--- test/integration/followers_notification_test.rb
+++ test/integration/followers_notification_test.rb
@@ -5,22 +5,23 @@ class FollowersNotificationTest < ActionController::IntegrationTest
 
   context "crossblaim creates a new convo" do
     should "notify followers" do
+      # login
       post_via_redirect "/user_session", :user_session => { :login => "crossblaim", :password => "secret" }
       assert_response :success
+      # create a new convo
       post_via_redirect "/conversations", :conversation => { :name => 'new crossblaim convo', :description => 'test convo'}
       assert_response :success
       assert_equal "Conversation was successfully created.", flash[:notice]
       @messages = users(:crossblaim).messages.find(:all, :conditions => ['system_message = ?', true], :limit => 2)
       assert_equal 2, @messages.length
-      get "/conversations/#{conversations(:dmitry_personal_convo).id}"
-      assert_response :success
-      assert_match(/this is a personal convo for dmitry/, @response.body)
-      # assert_match(/created a new convo\:/, @response.body)
+      # the actual messages for this convo are requested in js after tha page loads
+      get "/conversations/#{conversations(:dmitry_personal_convo).id}.js"
+      assert_match(/created a new convo\:/, @response.body)
       assert_match(/>new crossblaim convo<\/a>/, @response.body)
-      get "/conversations/#{conversations(:akira_personal_convo).id}"
+      # the actual messages for this convo are requested in js after tha page loads
+      get "/conversations/#{conversations(:akira_personal_convo).id}.js"
       assert_response :success
-      assert_match(/this is a personal convo for akira/, @response.body)
-      # assert_match(/created a new convo\:/, @response.body)
+      assert_match(/created a new convo\:/, @response.body)
       assert_match(/>new crossblaim convo<\/a>/, @response.body)
     end  
   end

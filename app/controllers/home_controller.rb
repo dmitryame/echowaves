659d55a3279735f924bf399326a4138b28435216
dmitryame <dmitryme@gmail.com>
2009-05-28 16:38:12 -0400

    filtering private convos from popular convo list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     @users = User.active.find(:all, :order => 'created_at DESC', :limit => 28)
     @recent_convos = Conversation.non_private.not_personal.find(:all, :order => 'created_at DESC', :limit => 10)
-    @popular_convos = Conversation.most_popular
+    @popular_convos = Conversation.non_private.most_popular
     @total_convos = Conversation.count
     @total_messages = Message.published.count
     @total_users = User.active.count

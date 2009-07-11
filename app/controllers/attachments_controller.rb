47e89e188bde7ec1998891906a5c3758f41b8b2f
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-07-11 10:30:00 +0200

    Add comment

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,11 +1,11 @@
 class AttachmentsController < ApplicationController
   before_filter :find_message_and_check_read_access
   def show
-    if USE_X_ACCEL_REDIRECT
+    if USE_X_ACCEL_REDIRECT # nginx
       head(:x_accel_redirect => @message.attachment.path.sub(RAILS_ROOT,''),  
          :content_type => @message.attachment_content_type,  
          :content_disposition => "inline")
-    else     
+    else # apache or others     
       send_file @message.attachment.path, :type => @message.attachment_content_type, :disposition => 'inline', :x_sendfile => USE_X_SENDFILE
     end
   end

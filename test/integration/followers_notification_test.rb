3872bd04bf80d984315af317da090c038290b1e0
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-07-21 22:13:21 +0200

    Fix defered tests

--- test/integration/followers_notification_test.rb
+++ test/integration/followers_notification_test.rb
@@ -2,7 +2,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class FollowersNotificationTest < ActionController::IntegrationTest
   fixtures :users, :conversations, :subscriptions
-
+  setup do
+    User.any_instance.stubs(:invite).returns(true)
+  end
   context "crossblaim creates a new convo" do
     should "notify followers" do
       # login
@@ -10,26 +12,20 @@ class FollowersNotificationTest < ActionController::IntegrationTest
       assert_response :success
       # create a new convo
       post_via_redirect "/conversations", :conversation => { :name => 'new crossblaim convo', :description => 'test convo'}
-      # TODO: defered 
-      # assert_response :success
-      # TODO: defered 
-      # assert_equal "Conversation was successfully created.", flash[:notice]
+      assert_response :success
+      assert_equal "Conversation was successfully created.", flash[:notice]
       @messages = users(:crossblaim).messages.find(:all, :conditions => ['system_message = ?', true], :limit => 2)
-      # assert_equal 2, @messages.length
+      assert_equal 2, @messages.length
       # the actual messages for this convo are requested in js after the page loads
       get "/conversations/#{conversations(:dmitry_personal_convo).id}/messages/system_messages.json"    
       assert_response :success
-      # TODO: defered 
-      # assert_match(/created a new convo\:/, @response.body)
-      # TODO: defered 
-      # assert_match(/>new crossblaim convo<\/a>/, @response.body)
+      assert_match(/created a new convo\:/, @response.body)
+      assert_match(/>new crossblaim convo<\/a>/, @response.body)
       # the actual messages for this convo are requested in js after the page loads
       get "/conversations/#{conversations(:akira_personal_convo).id}/messages/system_messages.json"
       assert_response :success
-      # TODO: defered 
-      # assert_match(/created a new convo\:/, @response.body)
-      # TODO: defered 
-      # assert_match(/>new crossblaim convo<\/a>/, @response.body)
+      assert_match(/created a new convo\:/, @response.body)
+      assert_match(/>new crossblaim convo<\/a>/, @response.body)
     end  
   end
 

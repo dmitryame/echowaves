f69e8a858db8f148da30c56ac8f1855bda219f19
dmitryame <dmitry@custommode.com>
2009-12-14 19:16:38 -0500

    adding ssl support, at this point just testing out

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,5 @@
 class UserSessionsController < ApplicationController
+  ssl_required :new, :create unless Rails.env.development?
 
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy

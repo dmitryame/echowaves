658e3ee896153bed644b4c84ffc5c03d5881cc65
Claudio Perez Gamayo <crossblaim@gmail.com>
2010-01-23 13:08:58 +0100

    [home page] don't fail if the home convo or the test convo are not created yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-  def ssl_required? 
+  def ssl_required?
     true if USE_SSL
   end
-  
+
   # just a homepage
   def index
     @users = User.active.find(:all, :order => 'created_at DESC', :limit => 24)
@@ -11,13 +11,15 @@ class HomeController < ApplicationController
     @total_convos = Conversation.count
     @total_messages = Message.published.count
     @total_users = User.active.count
+    @home_convo = Conversation.find_by_id(HOME_CONVERSATION)
+    @test_convo = Conversation.find_by_id(TEST_CONVERSATION)
   end
-  
+
   def terms
   end
 
   def toggle_sound
-    session[:sound] = (session[:sound] == "on" ? "off" : "on") 
+    session[:sound] = (session[:sound] == "on" ? "off" : "on")
     redirect_to :back
   end
 end
